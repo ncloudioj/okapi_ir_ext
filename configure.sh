b7c17099adcd34fbfe7aa3e7028505ce3f4219a0
Nan Cloudio Jiang <njiang028@gmail.com>
2012-12-28 14:34:09 -0500

    add a new shell to set the environment of Okapi automatically

---
+++ configure.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+## 
+# auto-configure the settings of Okapi
+# 1. set the dot.cshrc
+# 2. set the databases directory
+# 3. put the 'source dot.cshrc' into .bashrc
+
+set -e
+
+DB_DIR="${PWD}/databases"
+BIB_DIR="${PWD}/bibfiles"
+DOT_DIR="${PWD}/dot-files"
+
+# Modify the dot.cshrc
+OKAPI_ROOT_DIR="${PWD}"
+DOT_FILE="${DOT_DIR}/dot.cshrc"
+if [[ -f "${DOT_FILE}" ]]; then
+    sed -i "/OKAPI_ROOT=/c\export OKAPI_ROOT=${OKAPI_ROOT_DIR}"\
+           "${DOT_FILE}"
+fi
+
+# Modify the databases meta files
+# Get all installed databases
+for db in $( awk  'BEGIN { FS=" " } /^[^#]/ {print $1}' \
+             "${DB_DIR}/db_avail" ) ; do
+    sed -i "/bib_dir=/c\bib_dir=${BIB_DIR}"\
+           "${DB_DIR}/${db}"
+    sed -i "/ix_stem=/c\ix_stem=${BIB_DIR}/${db}"\
+           "${DB_DIR}/${db}"
+done
+
+# Put the 'source dot.cshrc' to .bashrc or .zshrc
+config_rc () {
+    local RC_FILE
+    echo "Which rc file do you want to set?"
+    read -p "(1).bash_rc (2).zshrc :" rc
+    case ${rc} in
+        1)
+            RC_FILE=".bashrc" ;;
+        2)
+            RC_FILE=".zshrc" ;;
+        *)
+            echo "Unknown rc file, please type the # again"
+            config_rc
+    esac
+
+    local OKAPI_LABEL="# Okapi environment setting"
+    if [[ -z $(grep "${OKAPI_LABEL}" "${HOME}/${RC_FILE}") ]]; then
+        echo "source ${DOT_FILE} # Okapi environment setting" >> \
+             "${HOME}/${RC_FILE}"
+        echo "export PATH=${OKAPI_ROOT_DIR}/bin:\$PATH" >> \
+             "${HOME}/${RC_FILE}"
+    fi
+}
+
+config_rc
