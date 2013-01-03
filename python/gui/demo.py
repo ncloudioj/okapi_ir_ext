2a5b94b1153c9599516b05cb706d3ad9c311a6c7
Nan Cloudio Jiang <njiang028@gmail.com>
2013-01-03 10:05:15 -0500

    Add the function of importing query file
    Enable the user to import query file instead of typing it one by one

--- python/gui/demo.py
+++ python/gui/demo.py
@@ -10,6 +10,10 @@ ID_COM_DATABASE     = 0x03
 ID_TXT_QUERIES      = 0x04
 ID_TXT_RECORD       = 0x05
 ID_LIST_RECORD      = 0x06
+ID_BTN_INFILE       = 0x07
+
+wildcard = "Text file (*.txt)|*.txt|" \
+           "All files (*.*)|*.*"
 
 class OkapiApp(wx.Frame):
 
@@ -32,6 +36,8 @@ class OkapiApp(wx.Frame):
         self.tc1 = wx.TextCtrl(panel, ID_TXT_QUERIES)
         hbox1.Add(self.tc1, 1)
         btn1 = wx.Button(panel, ID_BTN_SEARCH, 'Search', size=(70, 30))
+        btn3 = wx.Button(panel, ID_BTN_INFILE, 'Import', size=(70, 30))
+        hbox1.Add(btn3, 0)
         hbox1.Add(btn1, 0)
         vbox.Add(hbox1, 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 10)
         vbox.Add((-1, 10))
@@ -75,6 +81,7 @@ class OkapiApp(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnClose, id=ID_BTN_EXIT)
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.Bind(wx.EVT_LISTBOX, self.OnSelect, id=ID_LIST_RECORD)
+        self.Bind(wx.EVT_BUTTON, self.OnImport, id=ID_BTN_INFILE)
 
         # init okapi
         self.okapi = OkapiBss()
@@ -106,7 +113,7 @@ class OkapiApp(wx.Frame):
         (nset, nposting) = self.okapi.search(queries.split(" "))
         #import pdb; pdb.set_trace()
         self.records = {}
-        for record in self.okapi.show(nset, 0, 10):
+        for record in self.okapi.show(nset, 0, nposting):
             posting = "Posting: %s, Weight: %.3f" % (record["posting"],
                     record["weight"])
             self.records[posting] = record["text"]
@@ -126,6 +133,22 @@ class OkapiApp(wx.Frame):
         dial = wx.MessageDialog(None, msg, 'Question', wx.OK |
                 wx.ICON_INFORMATION)
         dial.ShowModal()
+
+    def OnImport(self, event):
+        dlg = wx.FileDialog(
+                            self, message="Choose a file",
+                            defaultFile="",
+                            wildcard=wildcard,
+                            style=wx.OPEN | wx.CHANGE_DIR
+                            )
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            self.queries = []
+            with open(path, 'r') as f:
+                for line in f:
+                    [ self.queries.append(word) for word in
+                            line.rstrip('\n').split(' ') ]
+            self.tc1.SetValue(" ".join(self.queries))
 
 def main():
 
